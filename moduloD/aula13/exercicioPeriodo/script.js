62c184fc85e46f790d835724a85ff3ec5310b73a
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-04-25 01:41:07 -0300

    push exercício Hora do dia

---
+++ moduloD/aula13/exercicioPeriodo/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    let msg = document.querySelector('div#msg');
+    let img = document.querySelector('img#foto');
+    let data = new Date();
+    let hora = data.getHours().toString().padStart(2, '0');
+    let minuto = data.getMinutes().toString().padStart(2, '0');
+    let segundo = data.getSeconds().toString().padStart(2, '0');
+    msg.innerHTML= `<strong>Agora são ${hora}:${minuto}:${segundo}hrs.</strong>`;
+    if(hora >= 6 && hora < 12) {
+        //BOM DIA
+        img.src = 'manha.jpg';
+        document.body.style.background = '#e2cd9f';
+    } else if( hora >= 12 && hora < 18) {
+        //Boa tarde
+        img.src = 'tarde.jpg';
+        document.body.style.background = '#b9846f'
+    } else {
+        //Boa noite
+        img.src = 'noite.jpg';
+        document.body.style.background = '#515154';
+    }
+    setInterval(carregar, 1000);
+}
