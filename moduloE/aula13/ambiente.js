2f3fe962102039cf988d383cf5e159ebad0c5a12
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-04-26 00:32:03 -0300

    push aula13

---
+++ moduloE/aula13/ambiente.js
@@ -0,0 +1,22 @@
+/*
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+ */
+
+/*
+let cont = 1;
+while(cont <= 6) {
+    console.log(`Passo ${cont}`);
+    cont++
+}
+*/
+
+let cont = 1;
+do {
+    console.log(`Passo ${cont}`);
+    cont++
+}while(cont <= 6)
